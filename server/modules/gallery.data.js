b24ccaf1c4d4f773fe4e6b43c55a1e3c564f9ab8
Sharmarke Duale <sduale01@gmail.com>
2019-01-18 17:23:39 -0600

    complete base mode

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,6 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    {id: 2, path: 'images/P1GTR.jpg', description: 'Photo of a Mclaren P1GTR', likes: 2}
 ];
 
 module.exports = galleryItems;
