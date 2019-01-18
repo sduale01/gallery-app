22a33c0b0f7b2e7d61e6a5db110ef1c6b82e19d1
Sharmarke Duale <sduale01@gmail.com>
2019-01-18 17:01:21 -0600

    add a conditional render on the image

--- src/components/Gallery/GalleryItem.js
+++ src/components/Gallery/GalleryItem.js
@@ -1,11 +1,33 @@
 import React, {Component} from 'react';
 
 class GalleryItem extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isImage: true,
+        }
+    }
+    // "this " doesnt exist if you dont use arrow functions
+    handleColumnClick = () => {
+        this.setState({
+            isImage: !this.state.isImage,
+        });
+    }
+
+
+
     render() {
+        const isImage = this.state.isImage;
+        let thingToShow;
+        if(isImage) {
+            thingToShow = <img src={this.props.createImage.path} alt="white goat"/>
+        } else {
+            thingToShow = <div>{this.props.createImage.description}</div>
+        }   
         return (
             <tr key={this.props.i}>
-                <td><img src={this.props.createImage.path} alt="white goat"/></td>
-                <td>{this.props.createImage.description}</td>
+                <td onClick={this.handleColumnClick}>{thingToShow}</td>
                 <td>{this.props.createImage.likes}</td>
             </tr>
         );
